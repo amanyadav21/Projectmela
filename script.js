a4fdd36adaeae6115b236839ceab039acb0fbaf7
amanyadav21 <amankumaryadav1029@gmail.com>
2025-04-12 12:25:15 +0530

    Almost every work are complete and run but some work are not complete just like responsive

--- script.js
+++ script.js
@@ -1,85 +1,67 @@
-// fetch("projects.json")
-//   .then(function (myresponse) {
-//     return myresponse.json();
-//   })
-
-//   .then(function (mydata) {
-//     mydata.forEach(function (projectItem) {
-//         // Main Container
-//       let project = document.querySelector("#project");
-
-//     //   Project ke liye div
-//       let projectlist = document.querySelector("#projectlist");
-
-//       let img = document.querySelector("#project-img")
-//       img.src = projectItem.
-      
-//       let title = document.querySelector("#project-title");
-//       let para = document.querySelector("#project-p");
-
-//       let gitbtn = document.querySelector("#github-btn");
-//       let livebtn = document.querySelector("#live-btn");
-//     });
-//   });
-
-
 fetch("projects.json")
   .then(function (myresponse) {
     return myresponse.json();
   })
   .then(function (mydata) {
-    let project = document.querySelector("#project");
+    let project = document.getElementById("project");
 
     mydata.forEach(function (projectItem) {
-      // Create parent div for each project
       let projectlist = document.createElement("div");
-      projectlist.classList.add("projectlist");
+      // projectlist.style.backgroundColor = 'red';
 
-      // Create and set image
+      // Image
       let img = document.createElement("img");
+      img.classList.add("p-img");
       img.src = projectItem.image;
       img.alt = projectItem.title;
-      img.classList.add("project-img");
 
-      // Create and set title
+      // Hr-line
+      let hrline = document.createElement("hr");
+      hrline.classList.add("hrline");
+
+      // Title
       let title = document.createElement("h1");
-      title.innerText = projectItem.title;
-      title.classList.add("project-title");
+      title.classList.add("p-title");
+      title.textContent = projectItem.title;
 
-      // Create and set paragraph
+      // Paragraph
       let para = document.createElement("p");
-      para.innerText = projectItem.description;
-      para.classList.add("project-p");
+      para.classList.add("p-para");
+      para.textContent = projectItem.description;
 
-      // Create button container
-      let buttonBox = document.createElement("div");
-      buttonBox.classList.add("button-class");
+      // // TechStack
+      // let techstack = document.createElement('p');
+      // techstack.classList.add('p-techstack');
+      // techstack.textContent = projectItem.techStack.join(', ');
 
-      // GitHub button
-      let gitbtn = document.createElement("button");
-      gitbtn.innerText = "GitHub";
-      gitbtn.onclick = function () {
-        window.open(projectItem.github, "_blank");
-      };
 
-      // Live button
-      let livebtn = document.createElement("button");
-      livebtn.innerText = "Live Demo";
-      livebtn.onclick = function () {
-        window.open(projectItem.live, "_blank");
-      };
+      // Button div
+      let buttondiv = document.createElement("div");
+      buttondiv.classList.add("buttondiv");
 
-      // Add buttons to button box
-      buttonBox.appendChild(gitbtn);
-      buttonBox.appendChild(livebtn);
+      let gitbtn = document.createElement("a");
+      gitbtn.classList.add("gitbtn");
+      gitbtn.textContent = "GitHub";
+      gitbtn.href = projectItem.github;
+      gitbtn.target = "_blank";
 
-      // Append everything to projectlist div
+      let livebtn = document.createElement("a");
+      livebtn.classList.add("livebtn");
+      livebtn.textContent = "Live";
+      livebtn.href = projectItem.live;
+      livebtn.target = "_blank";
+
+      buttondiv.appendChild(gitbtn);
+      buttondiv.appendChild(livebtn);
+
+      // Append all
       projectlist.appendChild(img);
+      projectlist.appendChild(hrline);
       projectlist.appendChild(title);
       projectlist.appendChild(para);
-      projectlist.appendChild(buttonBox);
+      // projectlist.appendChild(techstack);
+      projectlist.appendChild(buttondiv);
 
-      // Finally add projectlist to main container
       project.appendChild(projectlist);
     });
   });
